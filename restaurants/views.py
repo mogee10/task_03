b8655ee47d5ad6a06e33c4c39c05b59b3ff49931
Mohammed Algharaballi <malgharaballi@Mohammeds-MacBook-Pro.local>
2019-02-05 14:14:14 +0300

    submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,21 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    	'my_list': [
+    		{
+    			'restaurant_name': 'kofe',
+    			'food_type': 'coffee'
+    		},
+    		{
+    			'restaurant_name': 'mcdonalds',
+    			'food_type': 'junk food'
+    		},
+    		{
+    			'restaurant_name': 'joa',
+    			'food_type': 'sushi'
+    		},
+
+    	]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +29,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    	'my_object':{
+    	'restaurant_name': 'arabica',
+    	'food_type': 'yummy stuff',
+    	}
 
     }
     return render(request, 'detail.html', context)
